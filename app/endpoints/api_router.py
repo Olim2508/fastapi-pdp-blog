b5298c4d754233b6c93d24398bd910e3c2d06b5c
Olim2508 <rahmatovolim3@gmail.com>
2023-06-16 08:24:16 +0500

    crud and schemas for category model

--- app/endpoints/api_router.py
+++ app/endpoints/api_router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from endpoints import posts
+from endpoints import posts, category
 
 main_prefix = '/api/v1'
 
 router = APIRouter(prefix=main_prefix)
+router.include_router(category.router, prefix='/category', tags=['Category'])
 router.include_router(posts.router, prefix='/posts', tags=['Post'])
 
