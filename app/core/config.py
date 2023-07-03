87b9584c1093e1c370b4695eeab4ffce8a59277d
Olim2508 <rahmatovolim3@gmail.com>
2023-07-03 10:17:19 +0500

    create refresh token function

--- app/core/config.py
+++ app/core/config.py
@@ -15,8 +15,10 @@ class Settings(BaseSettings):
 
     API_MAIN_PREFIX: str = '/api/v1'
     SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_REFRESH_KEY: str = secrets.token_urlsafe(32)
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 5
     # 60 minutes * 24 hours * 8 days = 8 days
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
 
     EMAIL_TEMPLATES_DIR: str = "/app/email-templates/build"
     EMAILS_ENABLED: bool = False
--- app/core/security.py
+++ app/core/security.py
@@ -32,3 +32,14 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: timedelta = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=config.REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, config.REFRESH_TOKEN_EXPIRE_MINUTES, ALGORITHM)
+    return encoded_jwt
