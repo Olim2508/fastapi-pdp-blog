7651cc322436a98091695243b09eba6f12181164
Olim2508 <rahmatovolim3@gmail.com>
2023-07-05 09:38:58 +0500

    added refresh token to login api response

--- app/api/api_v1/endpoints/login.py
+++ app/api/api_v1/endpoints/login.py
@@ -26,13 +26,15 @@ def login_access_token(db: Session = Depends(deps.get_db), form_data: OAuth2Pass
     elif not crud.user.is_active(user):
         raise HTTPException(status_code=400, detail="Inactive user")
     access_token_expires = timedelta(minutes=config.ACCESS_TOKEN_EXPIRE_MINUTES)
+    refresh_token_expires = timedelta(minutes=config.REFRESH_TOKEN_EXPIRE_MINUTES)
     return {
         "access_token": security.create_access_token(user.id, expires_delta=access_token_expires),
+        "refresh_token": security.create_refresh_token(user.id, expires_delta=refresh_token_expires),
         "token_type": "bearer",
     }
 
 
-@router.post("/login/test-token", response_model=schemas.User)
+@router.post("/login/token/verify", response_model=schemas.User)
 def test_token(current_user: models.User = Depends(deps.get_current_user)) -> Any:
     """
     Test access token
