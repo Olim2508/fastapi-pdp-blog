1c547cc77865005a098fcced744c1d647446f6c7
Olim2508 <rahmatovolim3@gmail.com>
2023-07-20 18:44:53 +0500

    wait db fix

--- docker/dev/web/entrypoints/entrypoint.sh
+++ docker/dev/web/entrypoints/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-python backend_pre_start.py
+#python backend_pre_start.py
+
+echo "Waiting for postgres connection"
+
+while ! nc -z db 5432; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
 
 echo "Running alembic migrations..."
 alembic upgrade head
