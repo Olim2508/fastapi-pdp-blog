22645cc7ae75e62ea179abc3238b68c4a2157153
Olim2508 <rahmatovolim3@gmail.com>
2023-08-29 10:38:03 +0500

    added ordering by id added deleting posts on category delete

--- app/crud/crud_post.py
+++ app/crud/crud_post.py
@@ -21,18 +21,23 @@ class CRUDPost(CRUDBase[Post, PostCreate, PostUpdate]):
     def get_multi_by_category(self, db: Session, *, category_ids: List, skip: int = 0, limit: int = 100) -> Tuple:
         query = db.query(self.model)
         if not category_ids:
-            posts = query.offset(skip).limit(limit)
+            posts = query.order_by(self.model.id.desc()).offset(skip).limit(limit)
         else:
             posts = (
                 query(self.model)
                 .join(self.model.category)
                 .filter(Category.id.in_(category_ids))
+                .order_by(self.model.id.desc())
                 .offset(skip)
                 .limit(limit)
             )
         posts = posts.options(joinedload(self.model.category))
         count = posts.count()
         return posts.all(), count
+
+    def delete_posts_of_category(self, db: Session, *, category: Category):
+        db.query(self.model).filter(self.model.category_id == category.id).delete()
+        db.commit()
 
 
 post = CRUDPost(Post)
